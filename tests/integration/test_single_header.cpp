e2a5f7915247d8688051228df179e3414a1ae6a5
king-sj <2175616761@qq.com>
2025-11-23 20:28:56 +0800

    feat: add single header test

---
+++ tests/integration/test_single_header.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+#include "koroutine/koroutine.hpp"
+
+using namespace koroutine;
+
+TEST(IntegrationTest, SingleHeaderBasic) {
+  auto task = []() -> Task<int> { co_return 42; };
+
+  int result = 0;
+  auto wrapper = [&]() -> Task<void> { result = co_await task(); };
+
+  Runtime::block_on(wrapper());
+  EXPECT_EQ(result, 42);
+}
+
+TEST(IntegrationTest, SingleHeaderWhenAll) {
+  auto task1 = []() -> Task<int> { co_return 1; };
+  auto task2 = []() -> Task<int> { co_return 2; };
+
+  auto wrapper = [&]() -> Task<void> {
+    auto [r1, r2] = co_await when_all(task1(), task2());
+    EXPECT_EQ(r1, 1);
+    EXPECT_EQ(r2, 2);
+  };
+
+  Runtime::block_on(wrapper());
+}
